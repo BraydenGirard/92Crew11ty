b895c005aee98dbe4df5f52b98d4166f0735ae30
Brayden Girard <brayden@daydream.software>
2020-03-14 14:53:57 -0400

    Added autoplay video

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -120,3 +120,9 @@ buyButtonModal.addEventListener("click", function() {
         modal.classList.remove('open');
         e.preventDefault();
 });
+
+let playButton = document.getElementById("video-play-button");
+let player = document.getElementById("video-player");
+playButton.addEventListener( "click", function() {
+        player.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/8q3vHeOFfuw?autoplay=1" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+});
