416b3fb81e6e9348326ae452648eb2c918ff26b7
Brayden Girard <brayden@daydream.software>
2020-05-25 12:32:58 -0400

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,3 +3,9 @@
       'send_to': 'AW-737606010/gCXuCNWD4tABEPry298C',
       'transaction_id': ''
   });
+
+let playButton = document.getElementById("video-play-button");
+let player = document.getElementById("video-player");
+playButton.addEventListener( "click", function() {
+        player.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/8q3vHeOFfuw?autoplay=1" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+});
